991fb22a6186ce02fe4f510e2576de61b2f389fb
DipodDP <dipoddp@gmail.com>
2023-12-27 05:15:54 +0000

    fix: fly.toml update

--- userbot/sessions/redis_json.py
+++ userbot/sessions/redis_json.py
@@ -42,8 +42,11 @@ def get_redis_json(file_name: str):
 def save_redis_json(file_name: str):
     json_file = RedisJSON(config.bot_name, file_name, redis_connection).json
 
-    with open(file_name+'_backup', 'w', encoding='utf-8') as f:
-        json.dump(json_file, f, indent=2, ensure_ascii=False)
+    if json_file is None:
+        json_file = ''
+    else:
+        with open(file_name+'_backup', 'w', encoding='utf-8') as f:
+            json.dump(json_file, f, indent=2, ensure_ascii=False)
 
     return json_file
 
