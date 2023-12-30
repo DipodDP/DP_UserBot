7382678ea6a846b972aa07308930c1923c74789a
DipodDP <dipoddp@gmail.com>
2023-12-30 17:10:52 +0000

    feat(retrans): added resending to topics in supergroup

---
+++ userbot/handlers/resend_helpers.py
@@ -0,0 +1,57 @@
+from pyrogram.client import Client
+from pyrogram.types import Message
+from pyrogram.types.messages_and_media.message import Str
+
+
+async def message_attr_set(message: Message, channel_id: int):
+
+    caption = message.caption if message.caption is not None else ''
+    message.caption = Str(
+        f'{caption[:1020]}{"..." if len(caption) > 1023 else ""}')
+    if message.from_user is not None:
+        caption = f'{caption}\nFromessage:' \
+            f'{(" " + message.from_user.first_name) if message.from_user.first_name is not None else ""}' \
+            f'{(" " + message.from_user.last_name) if message.from_user.last_name is not None else ""}'
+
+    if message.sender_chat is not None:
+        message.sender_chat.has_protected_content = False
+        message.sender_chat.id = channel_id
+        message.sender_chat.title = f'{caption}\n{message.sender_chat.title}'
+
+    if message.chat is not None:
+        message.chat.id = channel_id
+        if message.chat.title is not None:
+            caption = f'{caption}\n{message.chat.title}'
+            message.chat.title = ''
+        message.chat.has_protected_content = False
+
+    message.text = Str(message.text + "\n" +
+                       caption) if message.text is not None else Str('')
+
+    return message
+
+
+async def handle_reply(
+    app: Client,
+    message: Message,
+    channel_id: int,
+    topic_message_id: int | None = None
+) -> int | None:
+
+    if 'reply_to_message_id' in repr(message):
+        reply_to_message: Message = await app.get_messages(
+            message.chat.id,
+            message.reply_to_message_id
+        )
+
+        reply_to_message = await message_attr_set(reply_to_message, channel_id)
+        sended_reply_to_message: Message = await reply_to_message.copy(
+            channel_id,
+            reply_to_message_id=topic_message_id
+        )
+        reply_to_message_id = sended_reply_to_message.id
+
+    else:
+        reply_to_message_id = topic_message_id
+
+    return reply_to_message_id
--- userbot/handlers/retrans.py
+++ userbot/handlers/retrans.py
@@ -7,131 +7,147 @@ from pyrogram.handlers.message_handler import MessageHandler
 from pyrogram.types import Message, User
 
 from userbot.app_init import config, LOGGER
+from userbot.handlers.resend_helpers import handle_reply, message_attr_set
 from userbot.sessions.redis_json import get_redis_json
 
 
 try:
     with open('retrans.json', encoding='utf-8') as f:
-        resend_configs = json.load(f)["resend_configs_list"]
+        resend_config = json.load(f)["resend_configs_list"]
 except FileNotFoundError:
     LOGGER.warning(
         "No local retrans.json"
     )
     try:
-        resend_configs = get_redis_json('retrans.json')["resend_configs_list"]
+        resend_config = get_redis_json(
+            'retrans.json')["resend_configs_list"]
     except Exception as e:
         LOGGER.error(
-            f"Unable to get retranslation config file!\n{e}"
+            f"Unable to get retranslation channel_configig file!\n{e}"
         )
         sys.exit(1)
 
-# getting list of channels for resending, to cut out messages from them
-to_channels = list(map(lambda to: to["to_channel_id"], resend_configs))
+# getting list of channels for resending, to fetch messages from them
+to_channels = list(
+    map(lambda to: to["to_channel_id"], resend_config))
 group_id = 0
 
 
-async def message_attr_set(m: Message, channel_id: int):
-
-    caption = m.caption if m.caption is not None else ''
-    m.caption = f'{caption[:1020]}{"..." if len(caption) > 1023 else ""}'
-    if m.from_user is not None:
-        caption = f'{caption}\nFrom:' \
-            f'{(" " + m.from_user.first_name) if m.from_user.first_name is not None else ""}' \
-            f'{(" " + m.from_user.last_name) if m.from_user.last_name is not None else ""}'
-
-    if m.sender_chat is not None:
-        m.sender_chat.has_protected_content = False
-        m.sender_chat.id = channel_id
-        m.sender_chat.title = f'{caption}\n{m.sender_chat.title}'
-
-    if m.chat is not None:
-        m.chat.id = channel_id
-        if m.chat.title is not None:
-            caption = f'{caption}\n{m.chat.title}'
-            m.chat.title = ''
-        m.chat.has_protected_content = False
-
-    m.text = (m.text + "\n" + caption) if m.text is not None else None
-
-    return m
-
-
-async def resend(app: Client, message: Message, channel_id: int):
+async def resend(
+    app: Client,
+    message: Message,
+    to_channel_id: int,
+    topic_messasge_id: int | None
+):
     global group_id
 
-    m: Message = await app.get_messages(message.chat.id, message.id)
-
-    LOGGER.debug(m)
+    LOGGER.debug(message)
     # printing some info
     LOGGER.info(
         "-".join([
-            m.text.split('\n')[0] if m.text is not None else 'no text',
-            m.caption if m.caption is not None else 'no caption',
-            str(getattr(getattr(m, "chat", "no chat"), "id", "no id")),
-            str(getattr(getattr(m, "chat", "no chat"), "title", "no title")),
-            getattr(getattr(m, "from_user", "Noname"), "first_name", "Noname"),
-            str(m.reply_to_message_id)
+            message.text.split(
+                '\n')[0] if message.text is not None else 'no text',
+            message.caption if message.caption is not None else 'no caption',
+            str(getattr(getattr(message, "chat", "no chat"), "id", "no id")),
+            str(getattr(getattr(message, "chat", "no chat"), "title", "no title")),
+            getattr(getattr(message, "from_user", "Noname"),
+                    "first_name", "Noname"),
+            str(message.reply_to_message_id)
         ])
     )
 
-    if 'reply_to_message_id' in repr(message):
-        m = await message_attr_set(m, channel_id)
-        reply_to_msg: Message = await app.get_messages(message.chat.id,
-                                                       message.reply_to_message_id)
-        reply_to_msg = await message_attr_set(reply_to_msg, channel_id)
-        reply_to_sent: Message = await reply_to_msg.copy(channel_id)
-        reply_to_msg_id = reply_to_sent.id
-
-    else:
-        m = await message_attr_set(m, channel_id)
-        reply_to_msg_id = None
+    reply_to_message_id = await handle_reply(
+        app,
+        message,
+        to_channel_id,
+        topic_messasge_id
+    )
 
     try:
         if message.media_group_id is not None:
             if group_id != message.media_group_id:
                 group_id = message.media_group_id
-                await app.copy_media_group(channel_id,
-                                           message.chat.id, message.id,
-                                           reply_to_message_id=reply_to_msg_id)
+                sended_message = await app.copy_media_group(
+                    to_channel_id,
+                    message.chat.id, message.id,
+                    reply_to_message_id=reply_to_message_id
+                )
+
         else:
-            await m.copy(channel_id, reply_to_message_id=reply_to_msg_id)
+            message = await message_attr_set(message, to_channel_id)
+            sended_message = await message.copy(
+                to_channel_id,
+                reply_to_message_id=reply_to_message_id
+            )
 
     except ChatForwardsRestricted:
 
-        file = await app.download_media(m, in_memory=True)
+        file = await app.download_media(message, in_memory=True)
 
-        if m.document is not None:
-            await app.send_document(channel_id, file, caption=m.caption)
-        elif m.photo is not None:
-            await app.send_photo(channel_id, file, caption=m.caption)
-        elif m.voice is not None:
-            await app.send_voice(channel_id, file, caption=m.caption)
-        elif m.video_note is not None:
-            await app.send_video_note(channel_id, file)
-        elif m.audio is not None:
-            await app.send_audio(channel_id, file, caption=m.caption)
-        elif m.video is not None:
-            await app.send_video(channel_id, file, caption=m.caption)
-        elif m.animation is not None:
-            await app.send_animation(channel_id, file, caption=m.caption)
+        if message.document and file:
+            await app.send_document(
+                to_channel_id,
+                file,
+                caption=message.caption,
+                reply_to_message_id=reply_to_message_id
+            )
+        elif message.photo and file:
+            await app.send_photo(
+                to_channel_id,
+                file,
+                caption=message.caption,
+                reply_to_message_id=reply_to_message_id
+            )
+        elif message.voice and file:
+            await app.send_voice(
+                to_channel_id,
+                file,
+                caption=message.caption,
+                reply_to_message_id=reply_to_message_id
+            )
+        elif message.video_note and file:
+            await app.send_video_note(
+                to_channel_id,
+                file,
+                reply_to_message_id=reply_to_message_id
+            )
+        elif message.audio and file:
+            await app.send_audio(
+                to_channel_id,
+                file,
+                caption=message.caption,
+                reply_to_message_id=reply_to_message_id
+            )
+        elif message.video and file:
+            await app.send_video(
+                to_channel_id,
+                file,
+                caption=message.caption,
+                reply_to_message_id=reply_to_message_id
+            )
+        elif message.animation and file:
+            await app.send_animation(
+                to_channel_id,
+                file,
+                caption=message.caption,
+                reply_to_message_id=reply_to_message_id
+            )
 
 
 async def resend_dispatcher(app: Client, message: Message):
 
-    msg: Message = await app.get_messages(message.chat.id, message.id)
-
     # setting sender id if sender is chat
-    if not msg.empty:
-        sender_id = msg.sender_chat.id if 'from_user' not in repr(msg) else\
-            msg.from_user.id if msg.from_user is not None else 'No sender id'
-        msg_text = msg.text if 'text' in repr(
-            msg) else msg.caption if 'caption' in repr(msg) else 'no text'
+    if not message.empty:
+        sender_id = message.sender_chat.id if 'from_user' not in repr(message) else\
+            message.from_user.id if message.from_user is not None else 'No sender id'
+        message_text = message.text if 'text' in repr(
+            message) else message.caption if 'caption' in repr(message) else 'no text'
 
-        for conf in resend_configs:
-            for source_channel in conf["source_channels"]:
+        for channel_config in resend_config:
+            for source_channel in channel_config["source_channels"]:
 
-                if msg.chat.id == source_channel["id"] or source_channel["id"] is None:
-                    # getting ids from str for usernames and from config attributes
+                if message.chat.id == source_channel["id"] or source_channel["id"] is None:
+                    # getting ids from str for usernames and from channel_configig attributes
                     i = 0
                     while i < len(source_channel["from"]):
 
@@ -146,16 +162,30 @@ async def resend_dispatcher(app: Client, message: Message):
                                 source_channel["from"].pop(i)
                         i += 1
 
-                    if ((any(map(lambda s: s in str(msg_text).lower(), source_channel["key_w"])))
+                    if ((any(map(
+                        lambda s: s in str(message_text).lower(),
+                        source_channel["key_w"])))
                         or (source_channel["key_w"] == [])
-                        and (sender_id in source_channel["from"] or source_channel["from"] == [])
-                        and ("reply_markup" not in repr(msg)
-                             or ("url" in repr(msg.reply_markup) or "ReplyKeyboardRemove" in repr(msg.reply_markup))
-                             if "reply_markup" in repr(msg) else True)) \
-                            and ((all(map(lambda s: s not in msg_text, source_channel["stop_w"])))
-                                 or (source_channel["stop_w"] == [])):
+                        and (sender_id in source_channel["from"]
+                        or source_channel["from"] == [])
+                        and (
+                            "reply_markup" not in repr(message)
+                            or (
+                                "url" in repr(message.reply_markup)
+                                or "ReplyKeyboardRemove" in repr(message.reply_markup)
+                            ) if "reply_markup" in repr(message) else True)) \
+                            and (
+                            (all(map(
+                                lambda s: s not in message_text, source_channel["stop_w"]
+                            ))) or (source_channel["stop_w"] == [])):
 
-                        await resend(app, message, conf["to_channel_id"])
+                        await resend(
+                            app,
+                            message,
+                            channel_config['to_channel_id'],
+                            channel_config['topic_messasge_id']
+                            if 'topic_messasge_id' in channel_config else None
+                        )
 
 
 def register_retrans(app: Client):
